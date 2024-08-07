bb9546e1c308072cce1dad68de847d2cca87ed08
henriqtorresl <henriquetlandin@gmail.com>
2024-08-07 18:38:57 -0300

    adicionando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  pathMatch: "full", redirectTo: "home" },
+  { path: 'home', component: HomeComponent },
+  { path: 'auth', component: AuthComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
