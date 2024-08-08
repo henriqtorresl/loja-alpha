5bc49655bd88d12dbad7bdb9887fa4431b31639f
henriqtorresl <henriquetlandin@gmail.com>
2024-08-08 17:08:02 -0300

    design da pagina de autenticação finalizado

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
 
+  @ViewChild('loginRef') loginRef!: ElementRef;
+  @ViewChild('registerRef') registerRef!: ElementRef;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  register(): void {
+    this.loginRef.nativeElement.style.display = 'none';
+    this.registerRef.nativeElement.style.display = 'flex';
+
+    this.registerRef.nativeElement.classList.add('slide-in');
+  }
+
+  login(): void {
+    this.registerRef.nativeElement.style.display = 'none';
+    this.loginRef.nativeElement.style.display = 'flex';
+
+    this.loginRef.nativeElement.classList.add('slide-in');
+  }
 }
