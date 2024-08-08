941827ee5c48a6b2e1dad29d35a23428cba6fc3e
henriqtorresl <henriquetlandin@gmail.com>
2024-08-08 17:12:46 -0300

    add interfaces de autenticação

---
+++ src/app/interfaces/CreateUserDto.ts
@@ -0,0 +1,7 @@
+export interface CreateUserDto {
+    name: string;
+    taxNumber: string;
+    mail: string;
+    phone: string;
+    password: string;
+}
---
+++ src/app/interfaces/LoginDto.ts
@@ -0,0 +1,4 @@
+export interface LoginDto {
+    taxNumber: string;  // cpf
+    password: string;
+}
